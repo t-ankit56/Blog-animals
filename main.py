7793df3283c9b59795712a6851d1801f933a4297
tankit <t.ankit5691@gmail.com>
2021-08-09 00:41:55 +0530

    Final commit test 3

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, url_for, redirect, request, flash, abort
 from mail import Mail
 from flask_sqlalchemy import SQLAlchemy
-
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_user, LoginManager, login_required, current_user, logout_user
+from flask_login import login_user, LoginManager, login_required, current_user, logout_user, UserMixin
 from functools import wraps
 
 from sqlalchemy.ext.declarative import declarative_base
@@ -14,7 +15,7 @@ from flask_gravatar import Gravatar
 import os
 from dotenv import load_dotenv
 from forms import *
-from models import *
+
 
 load_dotenv()  # take environment variables from .env.
 
@@ -37,6 +38,44 @@ gravatar = Gravatar(app,
 
 login_manager = LoginManager()
 login_manager.init_app(app)
+
+
+class User(UserMixin, db.Model, Base):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(200), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    posts = relationship("Blog", back_populates="author")
+    comments = relationship("Comments", back_populates="user")
+
+
+class Blog(db.Model, Base):
+    __tablename__ = "blog"
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, ForeignKey('users.id'))
+    author = relationship("User", back_populates="posts")
+    title = db.Column(db.String(200), unique=True, nullable=False)
+    subtitle = db.Column(db.String(250), unique=True, nullable=False)
+    date = db.Column(db.String(50), nullable=False)
+    link = db.Column(db.String(200), nullable=False)
+    body = db.Column(db.String(), nullable=False)
+    img_url = db.Column(db.String(), nullable=True)
+    comments = relationship("Comments", back_populates="post")
+
+
+class Comments(db.Model, Base):
+    __tablename__ = "comments"
+    id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer, ForeignKey('blog.id'))
+    user_id = db.Column(db.Integer, ForeignKey('users.id'))
+    post = relationship("Blog", back_populates="comments")
+    user = relationship("User", back_populates="comments")
+    comment = db.Column(db.String(200), nullable=True)
+    date_time = db.Column(db.String(), nullable=False)
+
+
+# db.create_all()
 
 
 @login_manager.user_loader
